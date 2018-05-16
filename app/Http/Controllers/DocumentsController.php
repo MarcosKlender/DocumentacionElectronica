19de744b5d3aca46507db7e7955ad5b695cb2253
MarcosKlender <marcosklender@gmail.com>
2018-05-16 12:56:36 -0500

    Ahora los documentos PDF se visualizan antes de descargar. Los documentos generados ya no se almacenan en la carpeta pública.

--- app/Http/Controllers/DocumentsController.php
+++ app/Http/Controllers/DocumentsController.php
@@ -179,17 +179,6 @@ class DocumentsController extends Controller
         $file = "$base64->numero_documento.pdf";
         file_put_contents($file, $decoded);
 
-        if (file_exists($file))
-        {
-            header('Content-Description: File Transfer');
-            header('Content-Type: application/octet-stream');
-            header('Content-Disposition: attachment; filename="'.basename($file).'"');
-            header('Expires: 0');
-            header('Cache-Control: must-revalidate');
-            header('Pragma: public');
-            header('Content-Length: ' . filesize($file));
-            readfile($file);
-            exit;
-        }
+        return response()->file($file)->deleteFileAfterSend(true);
     }
 }
