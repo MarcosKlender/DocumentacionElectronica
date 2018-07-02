4db4e5d9da308268018a95338f4a24f9a9719f26
MarcosKlender <marcosklender@gmail.com>
2018-07-02 00:19:15 -0500

    Modificación de la migración de usuarios. Creación del seeder.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,24 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Foundation\Auth\User;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+    	$Emproservis = DB::table('de01_xml')->select('persona_nombre', 'ruc_cliente_proveedor')->distinct()->get();
+
+        foreach ($Emproservis as $client)
+        {
+            User::firstOrCreate
+            ([
+                'name' => $client->persona_nombre,
+                'email' => str_random(10),
+                'ruc_o_ci' => $client->ruc_cliente_proveedor,
+                'password' => bcrypt($client->ruc_cliente_proveedor)
+            ]);
+        }
     }
 }
